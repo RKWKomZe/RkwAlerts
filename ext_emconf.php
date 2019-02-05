f50ace32a9d6e4ec8b3d92a3801d7945f432f511
Steffen kroggel <kroggel@rkw.de>
2019-02-05 13:44:42 +0100

    [FIX] dependencies fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,13 +24,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '7.6.3',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.2.0-7.6.99',
-			'fluid' => '6.2.0-7.6.99',
-			'typo3' => '6.2.0-7.6.99',
-			'rkw_basics' => '7.6.10-7.6.99',
-			'rkw_mailer' => '7.6.10-7.6.99',
-			'rkw_registration' => '7.6.10-7.6.99',
-			'rkw_projects' => '7.6.10-7.6.99',
+			'extbase' => '7.6.0-8.7.99',
+			'fluid' => '7.6.0-8.7.99',
+			'typo3' => '7.6.0-8.7.99',
+			'rkw_basics' => '7.6.10-8.7.99',
+			'rkw_mailer' => '7.6.10-8.7.99',
+			'rkw_registration' => '7.6.10-8.7.99',
+			'rkw_projects' => '7.6.10-8.7.99',
 		),
 		'conflicts' => array(
 		),
