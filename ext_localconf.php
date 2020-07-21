e0cbb06bc6398f92ca2534ef0d87709f0e9bbf0d
Steffen Kroggel <kroggel@rkw.de>
2020-07-21 09:47:17 +0200

    [FIX] Template paths as array [FIX] Loading frontend context for cronjobs [FIX] Logger paths fixed

--- ext_localconf.php
+++ ext_localconf.php
@@ -81,7 +81,7 @@ call_user_func(
                 // add a FileWriter
                 'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => array(
                     // configuration for the writer
-                    'logFile' => 'typo3temp/logs/tx_rkwalerts.log'
+                    'logFile' => 'typo3temp/var/logs/tx_rkwalerts.log'
                 )
             ),
         );
