54c36d1fb87ff4531722f64c01b60abe731ff505
Maximilian Faessler <maximilian@faesslerweb.de>
2021-02-09 08:52:48 +0000

    [EDIT] ext_tables update

--- ext_tables.php
+++ ext_tables.php
@@ -6,43 +6,12 @@ call_user_func(
     {
 
         //=================================================================
-        // Register Plugin
-        //=================================================================
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            $extKey,
-            'Rkwalerts',
-            'RKW Alerts'
-        );
-
-        //=================================================================
         // Add Tables
         //=================================================================
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
             'tx_rkwalerts_domain_model_alerts'
         );
 
-        //=================================================================
-        // Add Flexform
-        //=================================================================
-        $extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
-        $pluginName = strtolower('Rkwalerts');
-        $pluginSignature = $extensionName.'_'.$pluginName;
-
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-            $pluginSignature,
-            'FILE:EXT:'.$extKey . '/Configuration/FlexForms/Alerts.xml'
-        );
-
-        //=================================================================
-        // Add TypoScript
-        //=================================================================
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-            $extKey,
-            'Configuration/TypoScript',
-            'RKW Alerts'
-        );
 
     },
     $_EXTKEY
