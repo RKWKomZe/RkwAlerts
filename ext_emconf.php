c3927155c7ff2709eda277400c61a988732d1fbf
Steffen Kroggel <kroggel@rkw.de>
2023-10-05 17:32:20 +0200

    [CHANGE] extending dependencies to T3 v10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,12 +24,12 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '9.5.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.5.0-9.5.99',
-            'core_extended' => '9.5.4-9.5.99',
-            'ajax_api' => '9.5.0-9.5.99',
-            'postmaster' => '8.7.55-8.7.99',
-			'fe_register' => '8.7.0-8.7.99',
-            'rkw_projects' => '8.7.0-8.7.99',
+			'typo3' => '9.5.0-10.4.99',
+            'core_extended' => '9.5.4-10.4.99',
+            'ajax_api' => '9.5.0-10.4.99',
+            'postmaster' => '9.5.0-10.4.99',
+			'fe_register' => '9.5.0-10.4.99',
+            'rkw_projects' => '9.5.0-10.4.99',
 		],
 		'conflicts' => [
 		],
